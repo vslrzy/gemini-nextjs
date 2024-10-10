7a87326b1219c2aac869722b9907498991ee31f4
vslrzy <vslrzy@gmail.com>
2024-10-10 11:51:05 +0400

    Functionality works, no design

--- src/app/page.js
+++ src/app/page.js
@@ -7,18 +7,55 @@ import {useState} from "react";
 const GeminiAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_KEY);
 
 export default function Page() {
-    // Input change events
-    const [inputValue, setInputValue] = useState("");
-
+    // // Input change events
+    const [inputValue, setInputValue] = useState('');
+    //
     const chnageInputValue = (e) => {
         setInputValue(e.target.value);
         console.log(inputValue);
     };
 
+    // Response from AI state
+    const [promptResponses, setPromptResponses] = useState([])
+
+    // Request AI function
+    const getResponseByPropmt = async () => {
+        try {
+            const model = GeminiAI.getGenerativeModel({model: "gemini-pro"});
+            const result = await model.generateContent(inputValue);
+            const response = await result.response;
+            const text = await response.text();
+
+            setPromptResponses([
+                ...promptResponses,
+                text
+            ])
+
+            setInputValue('')
+        }catch (error){
+            console.log(error)
+        }
+    }
+
+    const GenerateResponse = () => {
+        if(inputValue.length >= 1){
+            getResponseByPropmt()
+        }else {
+            alert("Type Something Please")
+        }
+    }
+
     return (
         <main>
             <div>
-                <input type="text" onChange={chnageInputValue}/>
+                <input className={"border-2"} type="text" placeholder={"Type something..."} value={inputValue}
+                       onChange={chnageInputValue}/>
+                <button onClick={GenerateResponse}>Generate</button>
+                {promptResponses && promptResponses.map((promptResponse, index) => (
+                    <div key={index}>
+                        <div>{promptResponse}</div>
+                    </div>
+                ))}
             </div>
         </main>
     );
